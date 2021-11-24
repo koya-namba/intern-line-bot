957a211a928a7366c12c5ef7a88d19cb2eac7ce9
koya-namba <namba.koya@arakawa-lab.com>
2021-11-24 13:07:59 +0900

    [change] webhook_controller.rb

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,4 +1,7 @@
+require 'json'
 require 'line/bot'
+require 'net/http'
+require 'uri'
 
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
@@ -10,9 +13,24 @@ class WebhookController < ApplicationController
     }
   end
 
+  # 飲食店検索
+  def food_search_api(place, food)
+    data = {
+      "key": "01458b22b6dce274",
+      "address": place,
+      "keyword": food
+    }
+    query = data.to_query
+    uri = URI("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?" + query)
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri)
+    res = http.request(req)
+    res_data = Hash.from_xml(res.body)
+    return res_data
+  end
+
   def callback
     body = request.body.read
-
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       head 470
@@ -24,11 +42,35 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
+          text = event.message['text'].split("、")
+          place = text[0]
+          food = text[1]
+          res_data = food_search_api(place, food)
+
+          hello_content = <<~EOS
+          久しぶり〜！今度みんなでご飯でもどう？
+          #{place}でお勧めの#{food}を紹介するね！
+          EOS
+
           message = {
             type: 'text',
-            text: event.message['text']
+            text: hello_content
           }
           client.push_message(ENV['GROUP_ID'], message)
+
+          for i in 0..2 do
+            recommend_store = <<~EOS
+            店名： #{res_data['results']['shop'][i]['name']}
+            最寄駅： #{res_data['results']['shop'][i]['station_name']}
+            URL： #{res_data['results']['shop'][i]['urls']['pc']}
+            EOS
+            message = {
+              type: 'text',
+              text: recommend_store
+            }
+            client.push_message(ENV['GROUP_ID'], message)
+          end
+        
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
