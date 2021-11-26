89d93f9c0b37e77834a0155046ca57a340e9f3e4
koya-namba <namba.koya@arakawa-lab.com>
2021-11-26 13:39:43 +0900

    [change] migration, model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  validates :line_group_id, uniqueness: true
+  validates :line_group_id, uniqueness: true, presence: true
   has_many :group_users
   has_many :users, through: :group_users
   accepts_nested_attributes_for :group_users
